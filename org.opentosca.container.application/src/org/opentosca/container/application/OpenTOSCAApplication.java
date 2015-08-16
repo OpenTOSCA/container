b5aefbdee186c23a94a386c73e60830b759eba84
Kalman Kepes <kepeskn@studi.informatik.uni-stuttgart.de>
2015-08-17 00:54:02 +0200

    adds org.opentosca.container.application
    will be used by org.opentosca.container.product as hosting plugin

---
+++ org.opentosca.container.application/src/org/opentosca/container/application/OpenTOSCAApplication.java
@@ -0,0 +1,25 @@
+package org.opentosca.container.application;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * This class controls all aspects of the application's execution
+ */
+public class OpenTOSCAApplication implements IApplication {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
+	 */
+	public Object start(IApplicationContext context) throws Exception {
+		System.out.println("OpenTOSCA Container is starting..");
+		return IApplication.EXIT_OK;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#stop()
+	 */
+	public void stop() {
+		// nothing to do
+	}
+}
