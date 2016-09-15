a484f95eb78440b4004e4d2a28f2ea94053fc8cb
zimmerml <zimmerml@users.noreply.github.com>
2016-09-15 18:11:40 +0200

    CORS filter implementation added

---
+++ org.opentosca.containerapi/src/org/opentosca/containerapi/CorsFilter.java
@@ -0,0 +1,42 @@
+package org.opentosca.containerapi;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CorsFilter implements Filter {
+	
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		if (req.getHeader("Origin") != null) {
+			res.addHeader("Access-Control-Allow-Origin", "*");
+			res.addHeader("Access-Control-Expose-Headers", "X-Cache-Date, X-Atmosphere-tracking-id");
+		}
+		
+		if ("OPTIONS".equals(req.getMethod())) {
+			res.addHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+			res.addHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Atmosphere-Framework, X-Cache-Date, X-Atmosphere-tracking-id, X-Atmosphere-Transport");
+			res.addHeader("Access-Control-Max-Age", "-1");
+		}
+		chain.doFilter(req, res);
+	}
+	
+	@Override
+	public void destroy() {
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+}
