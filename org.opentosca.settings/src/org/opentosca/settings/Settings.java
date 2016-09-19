b113338c3918264853770f8a5acb145afa85a52c
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2016-09-19 15:56:00 +0200

    allows to set the repository of the container trough system propertes. E.g. -DopenTOSCAPath=/home/ubuntu should use /home/ubuntu/openTOSCA as openTOSCA container repository
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>

--- org.opentosca.settings/src/org/opentosca/settings/Settings.java
+++ org.opentosca.settings/src/org/opentosca/settings/Settings.java
@@ -8,34 +8,33 @@ import org.osgi.framework.BundleContext;
 
 /**
  * Global OpenTOSCA Settings.
- * 
+ *
  */
 public class Settings implements BundleActivator {
-	
+
 	public final static String CONTAINER_API = "http://localhost:1337/containerapi";
 	public final static String CONTAINER_INSTANCEDATA_API = "http://localhost:1337/containerapi/instancedata";
-	
 	
 	// TODO: Use public static final variables instead, as in
 	// StaticTOSCANamespaces. The problems with the current approach is: (i)
 	// Full-text search to find usage instead of Java Reference Search. (ii) It
 	// is possible to references non-existing settings, which is not possible
-	// with static variables which are checked on compile time. 
-	
+	// with static variables which are checked on compile time.
+
 	private static Properties settings = new Properties();
 	private static BundleContext context;
-	
+
 	// Container Capabilities
 	private final static String containerCapabilities = "http://opentosca/planportabilityapi/rest, http://opentosca/containerapi";
-	
-	
+
+
 	static BundleContext getContext() {
 		return Settings.context;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
 	 * )
@@ -43,56 +42,61 @@ public class Settings implements BundleActivator {
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		Settings.context = bundleContext;
-		
+
 		// /////////////////// PATHS ///////////////////
-		
+
 		// contains data of OpenTOSCA that should be stored permanently
-		String openTOSCAPath = System.getProperty("java.io.tmpdir") + File.separator + "openTOSCA";
+		String openTOSCAPath = "";
+		if (System.getProperty("openTOSCAPath") == null) {
+			openTOSCAPath = System.getProperty("java.io.tmpdir") + File.separator + "openTOSCA";
+		} else {
+			openTOSCAPath = System.getProperty("openTOSCAPath") + File.separator + "openTOSCA";
+		}
 		
 		// contains data of OpenTOSCA that should be stored temporarily
 		Settings.setSetting("temp", openTOSCAPath + File.separator + "Temp");
-		
+
 		// Derby database location
 		Settings.setSetting("databaseLocation", openTOSCAPath + File.separator + "DB");
-		
+
 		// relative path where CSARs will be stored locally; used by the
 		// Filesystem storage provider
 		Settings.setSetting("csarStorePath", openTOSCAPath + File.separator + "CSARs");
-		
+
 		// /////////////////// URLS ///////////////////
-		
+
 		// URI of the ContainerAPI
-		Settings.setSetting("containerUri", CONTAINER_API);
-		
+		Settings.setSetting("containerUri", Settings.CONTAINER_API);
+
 		// URI of the DataInstanceAPI
 		Settings.setSetting("datainstanceUri", "http://localhost:1337/datainstance");
-		
+
 		// /////////////////// CSAR ///////////////////
-		
+
 		// extension of a CSAR file
 		Settings.setSetting("csarExtension", "csar");
-		
+
 		// relative path of IMPORTS directory in a CSAR file
 		Settings.setSetting("csarImportsRelPath", "IMPORTS");
-		
+
 		// relative path of Definitions directory in a CSAR file
 		Settings.setSetting("csarDefinitionsRelPath", "Definitions");
-		
+
 		// relative path where the TOSCA meta file is located in a CSAR file
 		Settings.setSetting("toscaMetaFileRelPath", "TOSCA-Metadata" + File.separator + "TOSCA.meta");
-		
+
 		// possible file extensions of a TOSCA file, separated by character ";"
 		Settings.setSetting("toscaFileExtensions", "xml;tosca;ste");
-		
+
 		// /////////////////// OTHERS ///////////////////
-		
+
 		// Container Capabilities
 		Settings.setSetting("containerCapabilities", Settings.containerCapabilities);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
@@ -100,7 +104,7 @@ public class Settings implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 		Settings.context = null;
 	}
-	
+
 	/**
 	 * @param setting - name of the setting
 	 * @return the value of setting with name <code>setting</code>
@@ -108,15 +112,15 @@ public class Settings implements BundleActivator {
 	public static String getSetting(String setting) {
 		return Settings.settings.getProperty(setting);
 	}
-	
+
 	/**
 	 * Stores a setting.
-	 * 
+	 *
 	 * @param setting - name of the setting
 	 * @param value - value of the setting
 	 */
 	public static void setSetting(String setting, String value) {
 		Settings.settings.setProperty(setting, value);
 	}
-	
+
 }
