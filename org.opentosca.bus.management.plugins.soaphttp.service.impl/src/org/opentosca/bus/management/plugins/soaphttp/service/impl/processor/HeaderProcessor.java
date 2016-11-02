7ade047126193508ede6b5586ebb477cdbb05b81
zimmerml <zimmerml@users.noreply.github.com>
2016-11-02 14:29:52 +0100

    ws-addressing ReplyTo fixed

--- org.opentosca.bus.management.plugins.soaphttp.service.impl/src/org/opentosca/bus/management/plugins/soaphttp/service/impl/processor/HeaderProcessor.java
+++ org.opentosca.bus.management.plugins.soaphttp.service.impl/src/org/opentosca/bus/management/plugins/soaphttp/service/impl/processor/HeaderProcessor.java
@@ -17,55 +17,56 @@ import org.xml.sax.SAXException;
 /**
  * Header-Processor of the Management Bus-SOAP/HTTP-Plug-in.<br>
  * <br>
- * 
+ *
  * Copyright 2013 IAAS University of Stuttgart <br>
  * <br>
- * 
+ *
  * This processor copies all self defined header of the exchange object into
  * SoapHeader of the outgoing Soap message.
- * 
- * 
- * 
+ *
+ *
+ *
  * @author Michael Zimmermann - zimmerml@studi.informatik.uni-stuttgart.de
- * 
+ *
  */
 public class HeaderProcessor implements Processor {
+	
 	
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		
 		@SuppressWarnings("unchecked")
 		CxfPayload<SoapHeader> payload = exchange.getIn().getBody(CxfPayload.class);
-		
+
 		Map<String, Object> headers = exchange.getIn().getHeaders();
 		for (Map.Entry<String, Object> entry : headers.entrySet()) {
 			
 			if (entry.getKey().equalsIgnoreCase("ReplyTo")) {
 				
-				String xml1 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ReplyTo " + "xmlns:wsa=\"http://www.w3.org/2005/08/addressing\">" + entry.getValue().toString() + "</ReplyTo>";
+				String xml1 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ReplyTo " + "xmlns:wsa=\"http://www.w3.org/2005/08/addressing\"><wsa:Address>" + entry.getValue().toString() + "</wsa:Address></ReplyTo>";
 				SoapHeader replyToSoapHeader = new SoapHeader(new QName("http://www.w3.org/2005/08/addressing", "ReplyTo"), DOMUtils.readXml(new StringReader(xml1)).getDocumentElement());
 				payload.getHeaders().add(replyToSoapHeader);
-				
+
 			} else if (entry.getKey().equalsIgnoreCase("MessageID")) {
 				
 				String xml2 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><MessageID " + "xmlns:wsa=\"http://www.w3.org/2005/08/addressing\">" + entry.getValue().toString() + "</MessageID>";
 				SoapHeader messageIdSoapHeader = new SoapHeader(new QName("http://www.w3.org/2005/08/addressing", "MessageID"), DOMUtils.readXml(new StringReader(xml2)).getDocumentElement());
 				payload.getHeaders().add(messageIdSoapHeader);
-				
+
 			} else {
 				
 				payload.getHeaders().add(this.getSoapHeader(entry.getKey(), entry.getValue().toString()));
-				
+
 			}
 		}
-		
+
 		exchange.getIn().setBody(payload);
-		
+
 	}
-	
+
 	/**
 	 * Returns a SoapHeader
-	 * 
+	 *
 	 * @param key of the header
 	 * @param content of the header
 	 * @return SoapHeader
@@ -85,7 +86,7 @@ public class HeaderProcessor implements Processor {
 			e.printStackTrace();
 		}
 		return null;
-		
+
 	}
-	
+
 }
