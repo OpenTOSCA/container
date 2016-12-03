4e3d67d9b557a7dd47cdf03986d1b78b001293b4
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2016-12-03 13:17:37 +0100

    fixes dependency issue with the bare mvn build
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>

--- org.opentosca.wineryconnector/src/org/opentosca/wineryconnector/WineryConnector.java
+++ org.opentosca.wineryconnector/src/org/opentosca/wineryconnector/WineryConnector.java
@@ -14,9 +14,8 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.opentosca.settings.Settings;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Copyright 2016 IAAS University of Stuttgart <br>
@@ -26,46 +25,46 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  */
 public class WineryConnector {
-
+	
 	DefaultHttpClient client = new DefaultHttpClient();
 	String wineryPath;
-
-
+	
+	
 	public WineryConnector() {
 		this.wineryPath = Settings.getSetting("openTOSCAWineryPath");
 		if (!this.wineryPath.endsWith("/")) {
 			this.wineryPath = this.wineryPath + "/";
 		}
 	}
-	
+
 	public String getWineryPath() {
 		return this.wineryPath;
 	}
-
+	
 	public List<QName> getServiceTemplates() {
 		List<QName> qnames = new ArrayList<QName>();
-
+		
 		try {
-
+			
 			HttpGet get = new HttpGet();
 			get.setHeader("Accept", "application/json");
 			get.setURI(new URI(this.wineryPath + "servicetemplates"));
 			HttpResponse resp = this.client.execute(get);
 			String jsonResponse = EntityUtils.toString(resp.getEntity());
-
+			
 			ObjectMapper mapper = new ObjectMapper();
-
+			
 			ArrayList<Object> obj = mapper.readValue(jsonResponse, ArrayList.class);
-
+			
 			for (Object jsonObj : obj) {
 				LinkedHashMap<String, String> hashMap = (LinkedHashMap<String, String>) jsonObj;
-
+				
 				String id = hashMap.get("id");
 				String namespace = hashMap.get("namespace");
-
+				
 				qnames.add(new QName(namespace, id));
 			}
-
+			
 		} catch (ClientProtocolException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -75,8 +74,8 @@ public class WineryConnector {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-
+		
 		return qnames;
 	}
-
+	
 }
