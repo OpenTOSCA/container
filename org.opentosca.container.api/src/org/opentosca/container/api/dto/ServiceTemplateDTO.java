0a1529a7965b6c754855eae701f262df934dd08a
miwurster <miwurster@googlemail.com>
2017-06-06 13:36:37 +0200

    Added 'id' field to CSAR and ServiceTemplate DTOs

--- org.opentosca.container.api/src/org/opentosca/container/api/dto/ServiceTemplateDTO.java
+++ org.opentosca.container.api/src/org/opentosca/container/api/dto/ServiceTemplateDTO.java
@@ -1,5 +1,6 @@
 package org.opentosca.container.api.dto;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -9,9 +10,20 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ServiceTemplateDTO extends ResourceSupport {
 	
+	private String id;
+	
 	private String name;
 	
 	
+	@XmlAttribute(name = "id")
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
+	}
+
 	@XmlElement(name = "Name")
 	public String getName() {
 		return this.name;
