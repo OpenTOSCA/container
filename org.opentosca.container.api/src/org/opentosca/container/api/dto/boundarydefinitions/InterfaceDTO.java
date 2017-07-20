b12c322f8fd932f8c9149a62db9a1cf8a6e2eedf
miwurster <miwurster@googlemail.com>
2017-07-20 17:29:15 +0200

    Map-based serialization of Operation DTOs

--- org.opentosca.container.api/src/org/opentosca/container/api/dto/boundarydefinitions/InterfaceDTO.java
+++ org.opentosca.container.api/src/org/opentosca/container/api/dto/boundarydefinitions/InterfaceDTO.java
@@ -1,6 +1,6 @@
 package org.opentosca.container.api.dto.boundarydefinitions;
 
-import java.util.List;
+import java.util.Map;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
@@ -11,28 +11,28 @@ import org.opentosca.container.api.dto.ResourceSupport;
 
 @XmlRootElement(name = "Interface")
 public class InterfaceDTO extends ResourceSupport {
-	
+
 	private String name;
+
+	private Map<String, OperationDTO> operations;
 	
-	private List<OperationDTO> operations;
-
-
+	
 	@XmlAttribute
 	public String getName() {
 		return this.name;
 	}
-
+	
 	public void setName(final String name) {
 		this.name = name;
 	}
-	
+
 	@XmlElement(name = "Operation")
 	@XmlElementWrapper(name = "Operations")
-	public List<OperationDTO> getOperations() {
+	public Map<String, OperationDTO> getOperations() {
 		return this.operations;
 	}
-	
-	public void setOperations(final List<OperationDTO> operations) {
+
+	public void setOperations(final Map<String, OperationDTO> operations) {
 		this.operations = operations;
 	}
 }
