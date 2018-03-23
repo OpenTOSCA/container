3bf01da8e50cdbf9d067a15b01593f296ceffead
Michael Wurster <miwurster@googlemail.com>
2018-03-23 13:00:42 +0100

    Add Winery model dependencies
    Use Winery feature as main dependency.
    Make unit-test a little more useful to verify basic functionality.

---
+++ org.opentosca.container.integration.tests/src/org/opentosca/container/core/WineryRepositoryTests.java
@@ -0,0 +1,34 @@
+package org.opentosca.container.core;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.winery.repository.backend.IRepository;
+import org.eclipse.winery.repository.backend.RepositoryFactory;
+import org.eclipse.winery.repository.configuration.FileBasedRepositoryConfiguration;
+import org.junit.Assert;
+import org.junit.Test;
+import org.opentosca.container.core.next.utils.Consts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WineryRepositoryTests {
+
+    private static Logger logger = LoggerFactory.getLogger(WineryRepositoryTests.class);
+
+    @Test
+    public void testTryOut() throws Exception {
+        final Path csarRoot = Paths.get(Consts.TMPDIR, "opentosca", "CSARs");
+        Files.createDirectories(csarRoot);
+        final DirectoryStream<Path> stream = Files.newDirectoryStream(csarRoot, entry -> Files.isDirectory(entry));
+        boolean hit = false;
+        for (final Path p : stream) {
+            final IRepository r = RepositoryFactory.getRepository(new FileBasedRepositoryConfiguration(p));
+            logger.info("{}", r.getUsedNamespaces());
+            hit |= true;
+        }
+        Assert.assertTrue( "no CSARs were available under " + csarRoot, hit);
+    }
+}
