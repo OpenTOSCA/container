4f46346ee5cca923bf12f676c9db31153d099153
Stefan <st155160@stud.uni-stuttgart.de>
2018-08-18 00:22:21 +0200

    Added debug messages for improved debugging

--- org.opentosca.container.api/src/org/opentosca/container/api/controller/CsarController.java
+++ org.opentosca.container.api/src/org/opentosca/container/api/controller/CsarController.java
@@ -215,8 +215,7 @@ public class CsarController {
 
     private Response handleCsarUpload(final String filename, final InputStream is) {
 
-    	String fixedName = filename.replaceAll(" ", "");
-        final File file = this.csarService.storeTemporaryFile(fixedName, is);
+        final File file = this.csarService.storeTemporaryFile(filename, is);
 
         CSARID csarId;
 
