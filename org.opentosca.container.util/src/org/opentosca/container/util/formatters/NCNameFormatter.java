a07f4e5284a58be18e97f32f3e575025464c3991
Stefan <st155160@stud.uni-stuttgart.de>
2018-08-20 03:56:06 +0200

    Added Logger and debug message

--- org.opentosca.container.util/src/org/opentosca/container/util/formatters/NCNameFormatter.java
+++ org.opentosca.container.util/src/org/opentosca/container/util/formatters/NCNameFormatter.java
@@ -1,7 +1,8 @@
 package org.opentosca.container.util.formatters;
 
 import org.opentosca.container.util.formatter.impl.Formatter;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Formats a given String into a valid NCName format.
  * {@link https://stackoverflow.com/questions/1631396/what-is-an-xsncname-type-and-when-should-it-be-used}
@@ -9,6 +10,8 @@ import org.opentosca.container.util.formatter.impl.Formatter;
  *
  */
 public class NCNameFormatter extends Formatter<String> {
+	
+	private final static Logger LOG = LoggerFactory.getLogger(NCNameFormatter.class);
 
 	@Override
 	public NCNameFormattingResult format(final String input) {
@@ -18,6 +21,7 @@ public class NCNameFormatter extends Formatter<String> {
 		output.replaceAll(":", "_");
 		
 		NCNameFormattingResult result = new NCNameFormattingResult(output);
+		LOG.debug("Successfully formatted input {} to output {}", input, output);
 		return result;
 	}
 
