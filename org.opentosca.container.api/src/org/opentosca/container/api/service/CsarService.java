316f152e8767a8eca43a4a9a17adf9dfce8dfb1b
Clemens Lieb <vogel612@gmx.de>
2018-11-03 22:31:26 +0100

    Drop most of CsarService to oblivion

--- org.opentosca.container.api/src/org/opentosca/container/api/service/CsarService.java
+++ org.opentosca.container.api/src/org/opentosca/container/api/service/CsarService.java
@@ -1,27 +1,8 @@
 package org.opentosca.container.api.service;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.ServerErrorException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.namespace.QName;
-
-import org.apache.commons.io.FileUtils;
-import org.eclipse.winery.model.selfservice.Application;
-import org.opentosca.container.core.common.UserException;
-import org.opentosca.container.core.engine.IToscaEngineService;
-import org.opentosca.container.core.model.csar.CSARContent;
 import org.opentosca.container.core.model.csar.id.CSARID;
 import org.opentosca.container.core.service.ICoreFileService;
 import org.opentosca.planbuilder.export.Exporter;
@@ -36,126 +17,6 @@ public class CsarService {
     private static Logger logger = LoggerFactory.getLogger(CsarService.class);
 
     private ICoreFileService fileService;
-
-    private IToscaEngineService engineService;
-
-    /**
-     * Loads all available CSARs as {@link CSARContent}
-     *
-     * @return Set of {@link CSARContent} objects
-     */
-    @Deprecated
-    public Set<CSARContent> findAll() {
-        logger.debug("Requesting all CSARs...");
-        final Set<CSARContent> csarSet = new HashSet<>();
-        for (final CSARID id : this.fileService.getCSARIDs()) {
-            try {
-                csarSet.add(this.findById(id));
-            }
-            catch (final Exception e) {
-                logger.error("Error while loading CSAR with ID \"{}\": {}", id, e.getMessage(), e);
-                throw new ServerErrorException(Response.serverError().build());
-            }
-        }
-        return csarSet;
-    }
-
-    /**
-     * Loads a CSAR as {@link CSARContent} by a given id
-     *
-     * @param id The id of the CSAR
-     * @return The CSAR as {@link CSARContent}
-     */
-    @Deprecated
-    public CSARContent findById(final CSARID id) {
-        logger.debug("Requesting CSAR \"{}\"...", id);
-        try {
-            return this.fileService.getCSAR(id);
-        }
-        catch (final UserException e) {
-            logger.info("CSAR \"" + id.getFileName() + "\" could not be found");
-            throw new NotFoundException("CSAR \"" + id.getFileName() + "\" could not be found");
-        }
-    }
-
-    /**
-     * Loads a CSAR as {@link CSARContent} by a given id
-     *
-     * @param id The id of the CSAR
-     * @return The CSAR as {@link CSARContent}
-     */
-    @Deprecated
-    public CSARContent findById(final String id) {
-        return this.findById(new CSARID(id));
-    }
-
-    /**
-     * Reads the self-service metadata of a CSAR and returns it as a Java object
-     *
-     * @param csarContent The content object of the CSAR
-     * @return The self-service metadata as Java object
-     */
-    @Deprecated
-    public Application getSelfserviceMetadata(final CSARContent csarContent) {
-        try (final InputStream is =
-            csarContent.getDirectory("SELFSERVICE-Metadata").getFile("data.xml").getFileAsInputStream()) {
-            final JAXBContext jaxbContext = JAXBContext.newInstance(Application.class);
-            final Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            return (Application) jaxbUnmarshaller.unmarshal(is);
-        }
-        catch (final Exception e) {
-            logger.error("Could not serialize data.xml from CSAR", e);
-            throw new ServerErrorException(Status.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    /**
-     * Returns a set of strings representing service templates contained in a CSAR
-     *
-     * @param id The id of the CSAR
-     * @return A Set of String objects representing service templates
-     */
-    @Deprecated
-    public Set<String> getServiceTemplates(final CSARID id) {
-        logger.debug("Requesting ServiceTemplates of CSAR \"{}\"...", id);
-        final List<QName> result = this.engineService.getServiceTemplatesInCSAR(id);
-        return result.stream().filter(Objects::nonNull).map(item -> item.toString()).collect(Collectors.toSet());
-    }
-
-    /**
-     * Utility to check if a given CSAR has a certain ServiceTemplate attached
-     *
-     * @param id The id of the CSAR
-     * @param name The QName as string of the ServiceTemplate
-     * @return true or false
-     */
-    @Deprecated
-    public boolean hasServiceTemplate(final CSARID id, final String name) {
-        return this.getServiceTemplates(id).contains(name);
-    }
-
-    @Deprecated
-    public File storeTemporaryFile(final String filename, final InputStream is) {
-
-        final File tempDirectory = new File(System.getProperty("java.io.tmpdir"));
-
-        // Make sure the temp directory exists
-        tempDirectory.mkdir();
-
-        // Determine temporary file location
-        final File file = new File(tempDirectory, filename);
-
-        // Write the input stream into the file
-        try {
-            FileUtils.copyInputStreamToFile(is, file);
-        }
-        catch (final IOException e) {
-            logger.error("Error writing temporary CSAR file: {}", e.getMessage(), e);
-            return null;
-        }
-
-        return file;
-    }
 
     /**
      * Checks whether the plan builder should generate a build plans.
@@ -189,9 +50,5 @@ public class CsarService {
 
     public void setFileService(final ICoreFileService fileService) {
         this.fileService = fileService;
-    }
-
-    public void setEngineService(final IToscaEngineService engineService) {
-        this.engineService = engineService;
     }
 }
--- org.opentosca.container.integration.tests/src/org/opentosca/container/integration/tests/CsarActions.java
+++ org.opentosca.container.integration.tests/src/org/opentosca/container/integration/tests/CsarActions.java
@@ -1,15 +1,16 @@
 package org.opentosca.container.integration.tests;
 
-import org.opentosca.container.api.service.CsarService;
+import org.opentosca.container.core.model.csar.CsarId;
+import org.opentosca.container.core.service.CsarStorageService;
 
 public final class CsarActions {
 
-  public static boolean hasCsar(final String id) {
-    final CsarService service = ServiceTrackerUtil.getService(CsarService.class);
-    return service.findById(id) != null;
-  }
+    public static boolean hasCsar(final String id) {
+        final CsarStorageService service = ServiceTrackerUtil.getService(CsarStorageService.class);
+        return service.findById(new CsarId(id)) != null;
+    }
 
-  private CsarActions() {
-    throw new UnsupportedOperationException();
-  }
+    private CsarActions() {
+        throw new UnsupportedOperationException();
+    }
 }
