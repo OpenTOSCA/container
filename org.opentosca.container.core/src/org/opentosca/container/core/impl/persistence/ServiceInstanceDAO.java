a1fbd3318624111404d472f4368ea493ef899b16
Clemens Lieb <vogel612@gmx.de>
2018-11-30 20:31:14 +0100

    Drop unused ModelRepositoryService

--- org.opentosca.container.core/src/org/opentosca/container/core/impl/persistence/ServiceInstanceDAO.java
+++ org.opentosca.container.core/src/org/opentosca/container/core/impl/persistence/ServiceInstanceDAO.java
@@ -66,86 +66,14 @@ public class ServiceInstanceDAO {
 
     public List<ServiceInstance> getServiceInstances(final URI serviceInstanceID, final String serviceTemplateName,
                                                      final QName serviceTemplateID) {
-
         logger.info("Not Implemented: Service instances cannot be queried");
         return new ArrayList<>();
-
-        // this.init();
-        //
-        // // TODO: Use a query builder !!!
-        // // final Query getServiceInstancesQuery =
-        // // this.em.createNamedQuery(ServiceInstance.getServiceInstances);
-        // final TypedQuery<ServiceInstance> getServiceInstancesQuery;
-        //
-        // Integer internalID = null;
-        // if (serviceInstanceID != null) {
-        // internalID = IdConverter.serviceInstanceUriToID(serviceInstanceID);
-        // }
-        //
-        // // String serviceTemplateID_String = null;
-        // // if (serviceTemplateID != null) {
-        // // serviceTemplateID_String = serviceTemplateID.toString();
-        // // }
-        //
-        // if (internalID == null) {
-        // getServiceInstancesQuery = this.em.createQuery(
-        // "FROM ServiceInstance s WHERE s.serviceTemplateName = :serviceTemplateName AND
-        // s.serviceTemplateID = :serviceTemplateID",
-        // ServiceInstance.class);
-        // getServiceInstancesQuery.setParameter("serviceTemplateName", serviceTemplateName);
-        // getServiceInstancesQuery.setParameter("serviceTemplateID", serviceTemplateID);
-        // } else {
-        // getServiceInstancesQuery =
-        // this.em.createQuery("FROM ServiceInstance s WHERE s.id = :id", ServiceInstance.class);
-        // getServiceInstancesQuery.setParameter("id", internalID);
-        // }
-        //
-        // final List<ServiceInstance> queryResults = getServiceInstancesQuery.getResultList();
-        //
-        // return queryResults;
     }
 
     public List<ServiceInstance> getServiceInstances(final CSARID csarId, final QName serviceTemplateId,
                                                      final Integer serviceTemplateInstanceID) {
-
         logger.info("Not Implemented: Service instances cannot be queried");
         return new ArrayList<>();
-
-        // this.init();
-        //
-        // logger.debug("Try to get Service Template instance objects from persistence for CSAR \"{}\"
-        // Service Template \"{}\" Instance Id \"{}\"", csarId, serviceTemplateId,
-        // serviceTemplateInstanceID);
-        //
-        // // final Query getServiceInstancesQuery =
-        // // this.em.createNamedQuery(ServiceInstance.getServiceInstances);
-        // // TODO: Use a query builder or something else, but please refactor
-        // // this!!
-        //
-        // final String serviceTemplateName =
-        // InstanceDataServiceImpl.toscaEngineService.getNameOfReference(csarId, serviceTemplateId);
-        //
-        // TypedQuery<ServiceInstance> getServiceInstancesQuery = null;
-        //
-        // if (serviceTemplateInstanceID == null) {
-        // getServiceInstancesQuery = this.em.createQuery("FROM ServiceInstance s WHERE
-        // s.serviceTemplateName = :serviceTemplateName AND s.serviceTemplateID = :serviceTemplateID",
-        // ServiceInstance.class);
-        // getServiceInstancesQuery.setParameter("serviceTemplateName", serviceTemplateName);
-        // getServiceInstancesQuery.setParameter("serviceTemplateID", serviceTemplateId);
-        // } else {
-        // getServiceInstancesQuery = this.em.createQuery("FROM ServiceInstance s WHERE s.id = :id",
-        // ServiceInstance.class);
-        // getServiceInstancesQuery.setParameter("id", serviceTemplateInstanceID);
-        // }
-        //
-        // final List<ServiceInstance> queryResults = getServiceInstancesQuery.getResultList();
-        //
-        // logger.debug("Found {} instance objects for Service Template instance of CSAR \"{}\" Service
-        // Template \"{}\" Instance Id \"{}\"", queryResults.size(), csarId, serviceTemplateId,
-        // serviceTemplateInstanceID);
-        //
-        // return queryResults;
     }
 
     /**
@@ -155,7 +83,6 @@ public class ServiceInstanceDAO {
      * @param state to be set
      */
     public void setState(final ServiceInstance serviceInstance, final String state) {
-
         try {
             logger.info("ServiceInstance: {}", serviceInstance.toString());
             final Optional<ServiceTemplateInstance> o = this.repository.find(DaoUtil.toLong(serviceInstance.getId()));
