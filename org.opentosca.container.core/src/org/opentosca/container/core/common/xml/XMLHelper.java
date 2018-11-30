be6c661f791619180d037e1c3b8df40b4318e119
Clemens Lieb <vogel612@gmx.de>
2018-11-30 19:02:36 +0100

    Encapsulate common XML handling into Helper

---
+++ org.opentosca.container.core/src/org/opentosca/container/core/common/xml/XMLHelper.java
@@ -0,0 +1,32 @@
+package org.opentosca.container.core.common.xml;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public final class XMLHelper {
+
+    public static Document fromRootNode(Node node) {
+        if (node == null) {
+            return null;
+        }
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        try {
+            builder = dbf.newDocumentBuilder();
+        }
+        catch (ParserConfigurationException e) {
+            // YA we dun fucked up...
+            // LOGGER.error(e);
+            return null;
+        }
+        Document result = builder.newDocument();
+        Node imported = result.importNode(node, true);
+        result.appendChild(imported);
+        return result;
+    }
+    
+}
