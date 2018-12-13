0a3921b623d8a9e5eef9468045c18986a21d2331
Michael Wurster <miwurster@googlemail.com>
2018-12-13 22:16:24 +0100

    Add pom-first bundle structure

---
+++ bundles/org.opentosca.container.common/src/main/java/org/opentosca/container/common/Activator.java
@@ -0,0 +1,17 @@
+package org.opentosca.container.common;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    @Override
+    public void start(final BundleContext bundleContext) throws Exception {
+
+    }
+
+    @Override
+    public void stop(final BundleContext bundleContext) throws Exception {
+
+    }
+}
---
+++ bundles/org.opentosca.container.common/src/main/java/org/opentosca/container/common/MyAwesomeClass.java
@@ -0,0 +1,8 @@
+package org.opentosca.container.common;
+
+public abstract class MyAwesomeClass {
+
+	public static String sayHello() {
+		return "Hello!";
+	}
+}
