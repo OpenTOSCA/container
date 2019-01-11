fb04f1da755e1c409874a4803e6f673bd590cb5d
Clemens Lieb <vogel612@gmx.de>
2019-01-11 01:40:03 +0100

    Broaden LoggingExceptionMapper to deal with Throwables

--- org.opentosca.container.api/src/org/opentosca/container/api/config/LoggingExceptionMapper.java
+++ org.opentosca.container.api/src/org/opentosca/container/api/config/LoggingExceptionMapper.java
@@ -10,12 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Provider
-public class LoggingExceptionMapper implements ExceptionMapper<Exception> {
+public class LoggingExceptionMapper implements ExceptionMapper<Throwable> {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingExceptionMapper.class);
     
     @Override
-    public Response toResponse(Exception exception) {
+    public Response toResponse(Throwable exception) {
         logger.info("An exception was not handled!", exception);
         if (exception instanceof NotFoundException) {
             return Response.status(Status.NOT_FOUND).build();
