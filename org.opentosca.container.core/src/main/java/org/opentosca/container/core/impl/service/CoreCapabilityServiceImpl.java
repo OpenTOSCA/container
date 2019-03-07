4f12b68875c19269a805750a3d02a36413678ac6
Clemens Lieb <vogel612@gmx.de>
2019-03-07 08:14:23 +0100

    Rewrite CapabilityService to not unwrap EntityManager
    This sidesteps the issue of the unwrapping process being specific to the persistence-provider,
    formerly eclipselink, now h2 / hibernate.

--- org.opentosca.container.core/src/main/java/org/opentosca/container/core/impl/service/CoreCapabilityServiceImpl.java
+++ org.opentosca.container.core/src/main/java/org/opentosca/container/core/impl/service/CoreCapabilityServiceImpl.java
@@ -1,17 +1,13 @@
 package org.opentosca.container.core.impl.service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
+import org.opentosca.container.core.model.capability.Capability;
 import org.opentosca.container.core.model.capability.provider.ProviderType;
 import org.opentosca.container.core.next.jpa.EntityManagerProvider;
 import org.opentosca.container.core.service.ICoreCapabilityService;
@@ -33,49 +29,14 @@ import org.springframework.stereotype.Service;
  * @author Michael Zimmermann - zimmerml@studi.informatik.uni-stuttgart.de
  *
  */
-// FIXME get rid of this, or at least translate it to using jpa properly instead of raw Connections
 @Service
 public class CoreCapabilityServiceImpl implements ICoreCapabilityService {
     private static final Logger LOG = LoggerFactory.getLogger(CoreCapabilityServiceImpl.class);
-
-    private static final String CapabilitiesTable = "CAPABILITIES";
-    /**
-     * SQL queries for CapabilitiesTable: Creating the CapabilitiesTable, storing capabilities, getting
-     * already stored capabilities and deleting capabilities.
-     */
-    private static final String createCapabilitiesTable = "CREATE TABLE IF NOT EXISTS " + CapabilitiesTable
-        + " (Capability VARCHAR(1000) NOT NULL, ProviderName VARCHAR(1000) NOT NULL, ProviderType VARCHAR(1000) NOT NULL)";
-    private static final String storeCapabilities =
-        "INSERT INTO " + CapabilitiesTable + " (Capability, ProviderName, ProviderType) VALUES (?, ?, ?)";
-    private static final String getCapabilitiesByType =
-        "SELECT Capability, ProviderName FROM " + CapabilitiesTable + " WHERE ProviderType = ?";
-    private static final String getCapabilitiesByName =
-        "SELECT Capability FROM " + CapabilitiesTable + " WHERE ProviderName = ? AND ProviderType = ?";
-    private static final String deleteCapabilities = "DELETE FROM " + CapabilitiesTable + " WHERE ProviderName = ?";
 
     private final EntityManager em;
 
     public CoreCapabilityServiceImpl() {
         this.em = EntityManagerProvider.createEntityManager();
-        this.createCapabilitiesTable();
-    }
-    
-    /**
-     * Creates CapabilitiesTable if it not already exists.
-     */
-    private void createCapabilitiesTable() {
-        LOG.debug("Checking if table \"{}\" already exists...", this.CapabilitiesTable);
-        try {
-            this.em.getTransaction().begin();
-            Query createIfNE = em.createNativeQuery(createCapabilitiesTable);
-            createIfNE.executeUpdate();
-            LOG.debug("Table \"{}\" successfully created.", this.CapabilitiesTable);
-            this.em.getTransaction().commit();
-        }
-        catch (final RuntimeException exc) {
-            LOG.error("Database error - can't create CapabilitiesTable.", exc);
-            this.em.getTransaction().setRollbackOnly();
-        }
     }
 
     @Override
@@ -84,25 +45,14 @@ public class CoreCapabilityServiceImpl implements ICoreCapabilityService {
      */
     public void storeCapabilities(final List<String> capabilities, final String providerName,
                                   final ProviderType providerType) {
-        try {
-            LOG.debug("Storing \"{}\" capabilities of \"{}\" ...", providerType, providerName);
-            em.getTransaction().begin();
-            // FIXME can't be used
-            final Connection conn = em.unwrap(Connection.class);
+        LOG.debug("Storing \"{}\" capabilities of \"{}\" ...", providerType, providerName);
+        em.getTransaction().begin();
 
-            try (PreparedStatement pstmt = conn.prepareStatement(storeCapabilities)) {
-                for (final String capability : capabilities) {
-                    pstmt.setString(1, capability);
-                    pstmt.setString(2, providerName);
-                    pstmt.setString(3, providerType.name());
-                    pstmt.executeUpdate();
-                }
-            }
-            em.getTransaction().commit();
+        for (String capability : capabilities) {
+            Capability entity = new Capability(capability, providerName, providerType);
+            em.persist(entity);
         }
-        catch (final SQLException e) {
-            LOG.error("Database error - can't store \"{}\" capabilities in database.", providerType, e);
-        }
+        em.getTransaction().commit();
     }
 
     @Override
@@ -111,29 +61,17 @@ public class CoreCapabilityServiceImpl implements ICoreCapabilityService {
      */
     public Map<String, List<String>> getCapabilities(final ProviderType providerType) {
         LOG.debug("Getting \"{}\" capabilities...", providerType);
-        try {
-            em.getTransaction().begin();
-            // FIXME can't be used
-            final Connection conn = em.unwrap(Connection.class);
-            final Map<String, List<String>> capsMap = new HashMap<>();
-            try (PreparedStatement pstmt = conn.prepareStatement(getCapabilitiesByType)) {
-                pstmt.setString(1, providerType.name());
-                try (ResultSet queryRes = pstmt.executeQuery()) {
-                    // Fill map with Providers and their capabilities
-                    while (queryRes.next()) {
-                        capsMap.computeIfAbsent(queryRes.getString(2), a -> new ArrayList<String>())
-                            .add(queryRes.getString(1));
-                    }
-                }
-            }
-            LOG.debug("Getting \"{}\" capabilities successfully completed.", providerType);
-            em.getTransaction().commit();
-            return capsMap;
-        }
-        catch (final SQLException e) {
-            LOG.error("Database error - can't get \"{}\" capabilities from database.", providerType, e);
-            return null;
-        }
+        em.getTransaction().begin();
+
+        TypedQuery<Capability> query = em.createNamedQuery(Capability.byProviderType, Capability.class);
+        query.setParameter("providerType", providerType);
+
+        Map<String, List<String>> capabilitiesMap = query.getResultList().stream()
+            .collect(Collectors.groupingBy(Capability::getProviderName, Collectors.mapping(Capability::getCapability, Collectors.toList())));
+
+        LOG.debug("Getting \"{}\" capabilities successfully completed.", providerType);
+        em.getTransaction().commit();
+        return capabilitiesMap;
     }
 
     @Override
@@ -142,29 +80,18 @@ public class CoreCapabilityServiceImpl implements ICoreCapabilityService {
      */
     public List<String> getCapabilities(final String providerName, final ProviderType providerType) {
         LOG.debug("Getting \"{}\" capabilities of \"{}\"...", providerType, providerName);
-        try {
-            em.getTransaction().begin();
-            // FIXME can't be used
-            final Connection conn = em.unwrap(Connection.class);
-            final List<String> capabilities = new ArrayList<>();
-            try (PreparedStatement pstmt = conn.prepareStatement(getCapabilitiesByName)) {
-                pstmt.setString(1, providerName);
-                pstmt.setString(2, providerType.name());
-                try (ResultSet queryRes = pstmt.executeQuery()) {
-                    // Fill List with capabilities
-                    while (queryRes.next()) {
-                        capabilities.add(queryRes.getString(1));
-                    }
-                }
-            }
-            LOG.debug("Getting \"{}\" capabilities of \"{}\" successfully completed.", providerType, providerName);
-            em.getTransaction().commit();
-            return capabilities;
-        }
-        catch (final SQLException e) {
-            LOG.error("Database error - can't get capabilities of \"{}\" from database.", providerName, e);
-            return null;
-        }
+        em.getTransaction().begin();
+
+        TypedQuery<Capability> query = em.createNamedQuery(Capability.byProvider, Capability.class);
+        query.setParameter("providerType", providerType);
+        query.setParameter("providerName", providerName);
+
+        final List<String> capabilities = query.getResultList().stream()
+            .map(Capability::getCapability).collect(Collectors.toList());
+
+        LOG.debug("Getting \"{}\" capabilities of \"{}\" successfully completed.", providerType, providerName);
+        em.getTransaction().commit();
+        return capabilities;
     }
 
     @Override
@@ -172,19 +99,14 @@ public class CoreCapabilityServiceImpl implements ICoreCapabilityService {
      * {@inheritDoc}
      */
     public void deleteCapabilities(final String providerName) {
-        try {
-            LOG.debug("Deleting capabilities of \"{}\" ...", providerName);
-            em.getTransaction().begin();
-            // FIXME can't be used
-            final Connection conn = em.unwrap(Connection.class);
-            try (PreparedStatement pstmt = conn.prepareStatement(deleteCapabilities)) {
-                pstmt.setString(1, providerName);
-                pstmt.executeUpdate();
-            }
-            em.getTransaction().commit();
+        LOG.debug("Deleting capabilities of \"{}\" ...", providerName);
+        em.getTransaction().begin();
+        TypedQuery<Capability> query = em.createNamedQuery(Capability.byProvider, Capability.class);
+        query.setParameter("providerName", providerName);
+        for (ProviderType providerType : ProviderType.values()) {
+            query.setParameter("providerType", providerType);
+            query.getResultList().forEach(em::remove);
         }
-        catch (final SQLException e) {
-            LOG.error("Database error - can't delete capabilities of \"{}\" from database.", providerName, e);
-        }
+        em.getTransaction().commit();
     }
 }
---
+++ org.opentosca.container.core/src/main/java/org/opentosca/container/core/model/capability/Capability.java
@@ -0,0 +1,60 @@
+package org.opentosca.container.core.model.capability;
+
+import org.opentosca.container.core.model.capability.provider.ProviderType;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CAPABILITIES")
+@NamedQueries({
+  @NamedQuery(name = Capability.byProviderType, query = Capability.byProviderTypeQuery),
+  @NamedQuery(name = Capability.byProvider, query = Capability.byProviderQuery)
+})
+public class Capability {
+    static final String byProviderTypeQuery = "SELECT Capability c WHERE c.ProviderType = :providerType";
+    public static final String byProviderType = "Capability.ByProviderType";
+
+    static final String byProviderQuery = "SELECT Capability c WHERE c.ProviderType = :providerType AND c.ProviderName = :providerName";
+    public static final String byProvider = "Capability.ByProvider";
+
+    @Column(name = "Capability")
+    private String capability;
+    @Column(name = "ProviderName")
+    private String providerName;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ProviderType")
+    private ProviderType providerType;
+
+    // required for JPA
+    public Capability() {}
+
+    public Capability(String capability, String providerName, ProviderType providerType) {
+        this.capability = capability;
+        this.providerName = providerName;
+        this.providerType = providerType;
+    }
+
+    public String getCapability() {
+        return capability;
+    }
+
+    public void setCapability(String capability) {
+        this.capability = capability;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    public ProviderType getProviderType() {
+        return providerType;
+    }
+
+    public void setProviderType(ProviderType providerType) {
+        this.providerType = providerType;
+    }
+}
