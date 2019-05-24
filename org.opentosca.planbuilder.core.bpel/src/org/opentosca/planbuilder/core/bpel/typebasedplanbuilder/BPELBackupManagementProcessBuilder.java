4b9500196293983774a9ea37dd8ce6f244020d30
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2019-05-24 14:58:37 +0200

    Fix bug with wrong variables in backup plans

--- org.opentosca.planbuilder.core.bpel/src/org/opentosca/planbuilder/core/bpel/typebasedplanbuilder/BPELBackupManagementProcessBuilder.java
+++ org.opentosca.planbuilder.core.bpel/src/org/opentosca/planbuilder/core/bpel/typebasedplanbuilder/BPELBackupManagementProcessBuilder.java
@@ -230,7 +230,7 @@ public class BPELBackupManagementProcessBuilder extends AbstractManagementFeatur
                         found: for (final AbstractNodeTemplate nodeForMatching : nodesForMatching) {
                             for (final String propName : ModelUtils.getPropertyNames(nodeForMatching)) {
                                 if (param.getName().equals(propName)) {
-                                    inputs.put(param, BPELPlanContext.getVariable(saveStateUrlVarName));
+                                    inputs.put(param, context.getPropertyVariable(nodeTemplate, propName));
                                     break found;
                                 }
                             }
