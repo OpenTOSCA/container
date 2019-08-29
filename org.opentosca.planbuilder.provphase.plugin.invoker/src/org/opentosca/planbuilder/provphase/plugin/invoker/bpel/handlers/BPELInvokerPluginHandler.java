468e761cddbbec32e01bb1d4e2d88cca8708ddad
Tobias Mathony <mathony.tobias@gmail.com>
2019-08-29 10:45:14 +0200

    added todo, possible place to insert instanceRef as input param for operation

--- org.opentosca.planbuilder.provphase.plugin.invoker/src/org/opentosca/planbuilder/provphase/plugin/invoker/bpel/handlers/BPELInvokerPluginHandler.java
+++ org.opentosca.planbuilder.provphase.plugin.invoker/src/org/opentosca/planbuilder/provphase/plugin/invoker/bpel/handlers/BPELInvokerPluginHandler.java
@@ -22,7 +22,6 @@ import org.opentosca.container.core.tosca.convention.Properties;
 import org.opentosca.container.core.tosca.convention.Utils;
 import org.opentosca.planbuilder.core.bpel.context.BPELPlanContext;
 import org.opentosca.planbuilder.core.bpel.fragments.BPELProcessFragments;
-import org.opentosca.planbuilder.model.plan.AbstractPlan.PlanType;
 import org.opentosca.planbuilder.model.plan.bpel.BPELPlan;
 import org.opentosca.planbuilder.model.plan.bpel.BPELScope.BPELScopePhaseType;
 import org.opentosca.planbuilder.model.tosca.AbstractArtifactReference;
@@ -307,6 +306,9 @@ public class BPELInvokerPluginHandler {
             }
         }
 
+        // TODO: add maybe here the additional input param "instanceRef" if OperatingSystemInterface and
+        // abstract OS Node type found?
+
         // add external props to plan output message
         for (final String paraName : internalExternalPropsOutput.keySet()) {
             if (internalExternalPropsOutput.get(paraName) == null) {
@@ -340,7 +342,7 @@ public class BPELInvokerPluginHandler {
             // of removal
             // TIP this issue theoretically happens only with the "container deployment pattern" were a hosting
             // node has the operations needed to manage a component => different termination handling for such
-            // components is needed          
+            // components is needed
             assignNode =
                 this.resHandler.generateInvokerRequestMessageInitAssignTemplateAsNode(context.getCSARFileName(),
                                                                                       context.getServiceTemplateId(),
