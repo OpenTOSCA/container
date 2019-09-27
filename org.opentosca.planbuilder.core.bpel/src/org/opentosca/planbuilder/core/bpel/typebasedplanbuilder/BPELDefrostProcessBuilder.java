e31ba43d7cd7531f6154c8d8e9fb6371927f1811
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2019-09-27 16:38:22 +0200

    Fix/script execution (#111)
    * Enable script execution on the NodeTemplate with the script IA
    
    * Add search for transitively connected NodeTemplates to script execution
    
    * fix defrost plan:
    - add uploading DAs for
    - add execution of relations

--- org.opentosca.planbuilder.core.bpel/src/org/opentosca/planbuilder/core/bpel/typebasedplanbuilder/BPELDefrostProcessBuilder.java
+++ org.opentosca.planbuilder.core.bpel/src/org/opentosca/planbuilder/core/bpel/typebasedplanbuilder/BPELDefrostProcessBuilder.java
@@ -27,6 +27,7 @@ import org.opentosca.planbuilder.model.tosca.AbstractDefinitions;
 import org.opentosca.planbuilder.model.tosca.AbstractInterface;
 import org.opentosca.planbuilder.model.tosca.AbstractNodeTemplate;
 import org.opentosca.planbuilder.model.tosca.AbstractOperation;
+import org.opentosca.planbuilder.model.tosca.AbstractRelationshipTemplate;
 import org.opentosca.planbuilder.model.tosca.AbstractServiceTemplate;
 import org.opentosca.planbuilder.model.utils.ModelUtils;
 import org.opentosca.planbuilder.plugins.context.Property2VariableMapping;
@@ -208,6 +209,11 @@ public class BPELDefrostProcessBuilder extends AbstractDefrostPlanBuilder {
 
                 // generate code for the activity
                 this.bpelPluginHandler.handleActivity(context, bpelScope, nodeTemplate);
+            } else if (bpelScope.getRelationshipTemplate() != null) {
+                // handling relationshiptemplate
+                final AbstractRelationshipTemplate relationshipTemplate = bpelScope.getRelationshipTemplate();
+
+                this.bpelPluginHandler.handleActivity(context, bpelScope, relationshipTemplate);
             }
         }
     }
