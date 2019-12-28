89f92e16435adc964277b032861d1b6b19b90c7e
Clemens Lieb <vogel612@gmx.de>
2019-12-28 14:12:18 +0100

    Reinstate mqtt broker functionality

--- org.opentosca.container.war/src/main/java/org/opentosca/container/war/Config.java
+++ org.opentosca.container.war/src/main/java/org/opentosca/container/war/Config.java
@@ -1,7 +1,9 @@
 package org.opentosca.container.war;
 
+import org.opentosca.broker.mqtt.BrokerSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -15,11 +17,19 @@ public class Config implements ServletContextListener {
   @Override
   public void contextInitialized(ServletContextEvent event) {
     LOGGER.info("STARTING APPLICATION CONTEXT CONFIGURATION FOR OPENTOSCA CONTAINER API");
+    BrokerSupport mqttBroker = WebApplicationContextUtils
+      .getRequiredWebApplicationContext(event.getServletContext())
+      .getBean(BrokerSupport.class);
+    mqttBroker.start();
   }
 
   @Override
   public void contextDestroyed(ServletContextEvent event) {
     LOGGER.info("SHUTTING DOWN APPLICATION CONTEXT");
+    BrokerSupport mqttBroker = WebApplicationContextUtils
+      .getRequiredWebApplicationContext(event.getServletContext())
+      .getBean(BrokerSupport.class);
+    mqttBroker.stop();
   }
 
 }
