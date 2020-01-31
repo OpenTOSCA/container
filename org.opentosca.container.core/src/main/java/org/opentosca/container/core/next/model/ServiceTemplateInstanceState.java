a2ac401011d210965a5c6e62ad59e7f03a9b8ab1
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-01-31 17:27:43 +0100

    Use constants for plan states

--- org.opentosca.container.core/src/main/java/org/opentosca/container/core/next/model/ServiceTemplateInstanceState.java
+++ org.opentosca.container.core/src/main/java/org/opentosca/container/core/next/model/ServiceTemplateInstanceState.java
@@ -1,5 +1,5 @@
 package org.opentosca.container.core.next.model;
 
 public enum ServiceTemplateInstanceState {
-    INITIAL, CREATING, CREATED, DELETING, DELETED, ERROR, MIGRATING, MIGRATED
+    INITIAL, CREATING, CREATED, DELETING, DELETED, ERROR, MIGRATING, MIGRATED, FAILED
 }
