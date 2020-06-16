08cb6f16499c76618caa06fe20b490fdd0c8640f
Kálmán Képes <nyuuyn@users.noreply.github.com>
2020-06-16 14:04:24 +0200

    Feature/deletesits (#125)
    * fixes situation triggers to work with build plans
    
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
    
    * adds api methods to delete situations and situation triggers
    
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
    
    * Fix status code
    
    Co-authored-by: Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>

--- org.opentosca.container.api/src/org/opentosca/container/api/service/InstanceService.java
+++ org.opentosca.container.api/src/org/opentosca/container/api/service/InstanceService.java
@@ -671,6 +671,14 @@ public class InstanceService {
     public Collection<Situation> getSituations() {
         return this.sitRepo.findAll();
     }
+    
+    public boolean removeSituation(final Long situationId) {        
+        if(this.sitTrig.findSituationTriggersBySituationId(situationId).isEmpty()) {            
+            this.sitRepo.find(situationId).ifPresent(x -> this.sitRepo.remove(x));
+            return true;
+        }
+        return false;
+    }
 
     public Collection<SituationTrigger> getSituationTriggers() {
         return this.sitTrig.findAll();
@@ -726,6 +734,10 @@ public class InstanceService {
         }
 
         throw new NotFoundException("SituationTrigger <" + id + "> not found.");
+    }
+    
+    public void removeSituationTrigger(Long situationTriggerId) {
+        this.sitTrig.find(situationTriggerId).ifPresent(x -> this.sitTrig.remove(x));
     }
 
     public Collection<SituationTriggerInstance> geSituationTriggerInstances(final SituationTrigger trigger) {
