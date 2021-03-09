deaaeb2e94a0b2d404363e4177e2e93f1a8d3487
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2021-03-09 15:46:23 +0100

    adds testing of plan deployment for mytinytodo

--- org.opentosca.container.war/src/test/java/MyTinyToDoTest.java
+++ org.opentosca.container.war/src/test/java/MyTinyToDoTest.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 import javax.xml.namespace.QName;
 
 import org.eclipse.winery.accountability.exceptions.AccountabilityException;
 import org.eclipse.winery.common.configuration.RepositoryConfigurationObject;
+import org.eclipse.winery.model.tosca.TServiceTemplate;
 import org.eclipse.winery.repository.exceptions.RepositoryCorruptException;
 
 import org.junit.Assert;
@@ -44,6 +47,13 @@ public class MyTinyToDoTest extends CSARTest {
 
         Assert.assertTrue(this.csarService.generatePlans(this.csar));
 
-        this.storage.deleteCSAR(this.csar.id());
+        final List<TServiceTemplate> serviceTemplates = this.csar.serviceTemplates();
+        for (final TServiceTemplate serviceTemplate : serviceTemplates) {
+            this.control.invokePlanDeployment(this.csar.id(), serviceTemplate);
+        }
+
+
+
+        this.control.deleteCsar(this.csar.id());
     }
 }
