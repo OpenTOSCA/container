c0cb3c0ac5005dbd6c9aabc30a652b84384783da
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2021-03-11 18:48:33 +0100

    removes DOCKER_OPTS line in docker setup script

--- setupdockerapi.sh
+++ setupdockerapi.sh
@@ -1,5 +1,5 @@
 sudo sed -ie "s@ExecStart=\/usr\/bin\/dockerd -H fd:\/\/@ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ -H tcp:\/\/0.0.0.0:2375 -H unix:///var/run/docker.sock@g" /lib/systemd/system/docker.service
-sudo echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' >> /etc/default/docker
+#sudo echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' >> /etc/default/docker
 sudo systemctl daemon-reload
 sudo service docker restart
 wait 30
