73833b14b4dbfa01be341ebc0a05aecb818f3ef8
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2021-03-11 18:39:08 +0100

    add some privileges

--- setupdockerapi.sh
+++ setupdockerapi.sh
@@ -1,5 +1,5 @@
-sed -ie "s@ExecStart=\/usr\/bin\/dockerd -H fd:\/\/@ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ -H tcp:\/\/0.0.0.0:2375 -H unix:///var/run/docker.sock@g" /lib/systemd/system/docker.service
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' >> /etc/default/docker
-systemctl daemon-reload
-service docker restart
+sudo sed -ie "s@ExecStart=\/usr\/bin\/dockerd -H fd:\/\/@ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ -H tcp:\/\/0.0.0.0:2375 -H unix:///var/run/docker.sock@g" /lib/systemd/system/docker.service
+sudo echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' >> /etc/default/docker
+sudo systemctl daemon-reload
+sudo service docker restart
 wait 30
