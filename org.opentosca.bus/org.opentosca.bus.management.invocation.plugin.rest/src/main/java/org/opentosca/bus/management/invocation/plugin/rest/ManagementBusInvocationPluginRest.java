04f215973b16427a8c87b76e83b151b10571342b
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2021-05-27 15:20:59 +0200

    little codacy cleanup

--- org.opentosca.bus/org.opentosca.bus.management.invocation.plugin.rest/src/main/java/org/opentosca/bus/management/invocation/plugin/rest/ManagementBusInvocationPluginRest.java
+++ org.opentosca.bus/org.opentosca.bus.management.invocation.plugin.rest/src/main/java/org/opentosca/bus/management/invocation/plugin/rest/ManagementBusInvocationPluginRest.java
@@ -14,7 +14,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;;
+import javax.xml.parsers.ParserConfigurationException;
 
 import com.google.gson.JsonObject;
 import org.apache.camel.CamelContext;
