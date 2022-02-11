7278e4ad496e6504c795959f8085d038706472bd
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2022-02-11 14:55:08 +0100

    Handle capabilities with JPA repository

---
+++ org.opentosca.container.core/src/main/java/org/opentosca/container/core/next/model/Capability.java
@@ -0,0 +1,50 @@
+package org.opentosca.container.core.next.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.opentosca.container.core.model.capability.provider.ProviderType;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = Endpoint.TABLE_NAME)
+public class Capability extends PersistenceObject {
+
+    public static final String TABLE_NAME = "CAPABILITIES";
+
+    private static final long serialVersionUid = 684635434L;
+
+    @Column(name = "capability")
+    private String capability;
+
+    @Column(name = "providerName")
+    private String providerName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "providerType")
+    private ProviderType providerType;
+
+    @Override
+    public int hashCode() {
+        return capability.hashCode() ^ providerName.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Capability)) {
+            return false;
+        }
+        Capability other = (Capability) o;
+        return capability.equals(other.capability)
+            && providerName.equals(other.providerName)
+            && providerType.equals(other.providerType);
+    }
+}
